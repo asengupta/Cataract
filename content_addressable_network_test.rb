65da185a5a6504b3e9496126ef2474044105b3a0
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-12-31 12:30:08 +0530

    Adding left zone adjacency test

--- content_addressable_network_test.rb
+++ content_addressable_network_test.rb
@@ -40,5 +40,20 @@ class ZoneTest < Test::Unit::TestCase
 		assert_equal(false, z1.is_adjacent_to(z7))
 		assert_equal(true, z1.is_adjacent_to(z8))
 	end
+
+	def test_left_zone_adjacency
+		z1 = Zone.new({:x => 0.2, :y => 0.4}, {:x => 0.4, :y => 0.2})
+		z2 = Zone.new({:x => 0.1, :y => 0.15}, {:x => 0.15, :y => 0.1})
+		z3 = Zone.new({:x => 0.1, :y => 0.2}, {:x => 0.2, :y => 0.1})
+		z4 = Zone.new({:x => 0.1, :y => 0.3}, {:x => 0.2, :y => 0.1})
+		z5 = Zone.new({:x => 0.1, :y => 0.6}, {:x => 0.2, :y => 0.3})
+		z6 = Zone.new({:x => 0.1, :y => 0.6}, {:x => 0.2, :y => 0.1})
+
+		assert_equal(false, z1.is_adjacent_to(z2))
+		assert_equal(false, z1.is_adjacent_to(z3))
+		assert_equal(true, z1.is_adjacent_to(z4))
+		assert_equal(true, z1.is_adjacent_to(z5))
+		assert_equal(true, z1.is_adjacent_to(z6))
+	end
 end
 
