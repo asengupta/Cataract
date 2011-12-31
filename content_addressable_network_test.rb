44c3fe1e4118808bed57c79d4dd1d1d27199dff0
Avishek Sen Gupta <asgupta@thoughtworks.com>
2011-12-31 12:19:31 +0530

    Updating adjacency test

--- content_addressable_network_test.rb
+++ content_addressable_network_test.rb
@@ -1,9 +1,25 @@
 require './content_addressable_node'
+require 'rubygems'
+require 'test/unit'
 
-z1 = Zone.new({:x => 0.2, :y => 0.4}, {:x => 0.4, :y => 0.2})
-z2 = Zone.new({:x => 0.1, :y => 0.15}, {:x => 0.15, :y => 0.1})
-z3 = Zone.new({:x => 0.1, :y => 0.2}, {:x => 0.2, :y => 0.1})
+class ZoneTest < Test::Unit::TestCase
+	def test_zone_adjacency
+		z1 = Zone.new({:x => 0.2, :y => 0.4}, {:x => 0.4, :y => 0.2})
+		z2 = Zone.new({:x => 0.1, :y => 0.15}, {:x => 0.15, :y => 0.1})
+		z3 = Zone.new({:x => 0.1, :y => 0.2}, {:x => 0.2, :y => 0.1})
+		z4 = Zone.new({:x => 0.1, :y => 0.2}, {:x => 0.3, :y => 0.1})
+		z5 = Zone.new({:x => 0.3, :y => 0.2}, {:x => 0.8, :y => 0.1})
+		z6 = Zone.new({:x => 0.4, :y => 0.2}, {:x => 0.8, :y => 0.1})
+		z7 = Zone.new({:x => 0.45, :y => 0.2}, {:x => 0.8, :y => 0.1})
+		z8 = Zone.new({:x => 0.1, :y => 0.2}, {:x => 0.8, :y => 0.1})
 
-puts z1.is_adjacent_to(z2)
-puts z1.is_adjacent_to(z3)
+		assert_equal(false, z1.is_adjacent_to(z2))
+		assert_equal(false, z1.is_adjacent_to(z3))
+		assert_equal(true, z1.is_adjacent_to(z4))
+		assert_equal(true, z1.is_adjacent_to(z5))
+		assert_equal(false, z1.is_adjacent_to(z6))
+		assert_equal(false, z1.is_adjacent_to(z7))
+		assert_equal(true, z1.is_adjacent_to(z8))
+	end
+end
 
